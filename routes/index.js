2e5df6d4bd764c34a1b5c3b1fc0be8fb892dbe0e
SenkuDonda007 <kevadiyahet1234@gmail.com>
2023-11-07 09:02:00 -0500

    This is the Expres website with EJS views

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+// Route for Tom's page
+router.get('/', function(req, res, next) {
+  res.render('tom');
+});
+// Route for Tom's page
+router.get('/tom', function(req, res, next) {
+  res.render('tom');
+});
+
+// Route for Chris's page
+router.get('/chris', function(req, res, next) {
+  res.render('chris');
+});
+
+// Route for John's page
+router.get('/john', function(req, res, next) {
+  res.render('john');
+});
+
+// Route for Emma's page
+router.get('/emma', function(req, res, next) {
+  res.render('emma');
+});
+
+module.exports = router;
